9d914923c73d053216de5c8607e6f9bac007f368
jhott <ottjh@uc.mail.edu>
2024-10-12 16:42:13 -0400

    Probably final version

--- src/Main.java
+++ src/Main.java
@@ -13,67 +13,89 @@ public class Main
         String input2 = "";
         boolean incompleteField = false;
         String trash = "";
+        boolean playerContinue = true;
 
         System.out.println("Welcome to a game of Rock, Paper, Scissors");
         //Now to get player one's input and test it for validity
         do {
-            System.out.print("Player 1 please enter your move[R,P,S]: ");
-            input1 = in.nextLine();
+            do {
+                System.out.print("Player 1 please enter your move[R,P,S]: ");
+                input1 = in.nextLine();
 
-            if (input1.equalsIgnoreCase("r"))
-            {
-                System.out.println("Player 1 has chosen Rock.");
-                incompleteField = false;
-            }
-            else if (input1.equalsIgnoreCase("p"))
-            {
-                System.out.println("Player 1 has chosen Paper.");
-                incompleteField = false;
-            }
-            else if (input1.equalsIgnoreCase("s"))
-            {
-                System.out.println("Player 1 has chosen Scissors.");
-                incompleteField = false;
-            }
-            else
-            {
-                trash = input1;
-                System.out.println("Please enter a valid move [R,P,S] not: " + trash + ".");
-                incompleteField = true;
-            }
-        } while (incompleteField);
+                if (input1.equalsIgnoreCase("r")) {
+                    System.out.println("Player 1 has chosen Rock.");
+                    incompleteField = false;
+                } else if (input1.equalsIgnoreCase("p")) {
+                    System.out.println("Player 1 has chosen Paper.");
+                    incompleteField = false;
+                } else if (input1.equalsIgnoreCase("s")) {
+                    System.out.println("Player 1 has chosen Scissors.");
+                    incompleteField = false;
+                } else {
+                    trash = input1;
+                    System.out.println("Please enter a valid move [R,P,S] not: " + trash + ".");
+                    incompleteField = true;
+                }
+            } while (incompleteField);
 
-        incompleteField = false; //Resetting incomplete for the next loop
-        //System.out.println("We have escaped test loop 1"); // Helping to test exiting loop
+            incompleteField = false; //Resetting incomplete for the next loop
+            //System.out.println("We have escaped test loop 1"); // Helping to test exiting loop
 
-        //Now time for Player 2
-        do {
-            System.out.print("Player 2 please enter your move[R,P,S]: ");
-            input2 = in.nextLine();
+            //Now time for Player 2
+            do {
+                System.out.print("Player 2 please enter your move[R,P,S]: ");
+                input2 = in.nextLine();
 
-            if (input2.equalsIgnoreCase("r"))
-            {
-                System.out.println("Player 2 has chosen Rock.");
-                incompleteField = false;
-            }
-            else if (input2.equalsIgnoreCase("p"))
-            {
-                System.out.println("Player 2 has chosen Paper.");
-                incompleteField = false;
-            }
-            else if (input2.equalsIgnoreCase("s"))
-            {
-                System.out.println("Player 2 has chosen Scissors.");
-                incompleteField = false;
-            }
-            else
-            {
-                trash = input2;
-                System.out.println("Please enter a valid move [R,P,S] not: " + trash + ".");
-                incompleteField = true;
-            }
-        } while (incompleteField);
+                if (input2.equalsIgnoreCase("r")) {
+                    System.out.println("Player 2 has chosen Rock.");
+                    incompleteField = false;
+                } else if (input2.equalsIgnoreCase("p")) {
+                    System.out.println("Player 2 has chosen Paper.");
+                    incompleteField = false;
+                } else if (input2.equalsIgnoreCase("s")) {
+                    System.out.println("Player 2 has chosen Scissors.");
+                    incompleteField = false;
+                } else {
+                    trash = input2;
+                    System.out.println("Please enter a valid move [R,P,S] not: " + trash + ".");
+                    incompleteField = true;
+                }
+            } while (incompleteField);
 
-        //System.out.println("We have escaped test loop 2"); //testing for exiting loop again
+            //System.out.println("We have escaped test loop 2"); //testing for exiting loop again
+
+            if (input1.equalsIgnoreCase("r")) {
+                if (input2.equalsIgnoreCase("r")) {
+                    System.out.println("Both Players choose rock. It's a tie.");
+                } else if (input2.equalsIgnoreCase("p")) {
+                    System.out.println("Paper covers rock. Player 2 wins.");
+                } else {
+                    System.out.println("Rock breaks scissors. Player 1 wins.");
+                }
+            } else if (input1.equalsIgnoreCase("p")) {
+                if (input2.equalsIgnoreCase("r")) {
+                    System.out.println("Paper covers rock. Player 1 wins.");
+                } else if (input2.equalsIgnoreCase("p")) {
+                    System.out.println("Both Players choose paper. It's a tie.");
+                } else {
+                    System.out.println("Scissors cut paper. Player 2 wins.");
+                }
+            } else {
+                if (input2.equalsIgnoreCase("r")) {
+                    System.out.println("Rock breaks scissors. Player 2 wins.");
+                } else if (input2.equalsIgnoreCase("p")) {
+                    System.out.println("Scissors cut paper. Player 1 wins.");
+                } else {
+                    System.out.println("Both Players choose scissors. It's a tie.");
+                }
+            }
+
+            //now to ask the player if they want to continue playing
+            System.out.println("Enter [Q] to quit, otherwise enter anything else to play again: ");
+            if (in.nextLine().equalsIgnoreCase("q"))
+            {
+                playerContinue = false;
+            }
+        } while (playerContinue);
     }
 }
